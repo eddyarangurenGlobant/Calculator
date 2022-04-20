0d097d73ad14ea81fd4c263e78d9ef0983dbc156
eddyarangurenGlobant <eddy.aranguren@globant.com>
2022-04-20 00:03:58 -0500

    Adding the multiply and divide methods

--- src/Data/Calculator.java
+++ src/Data/Calculator.java
@@ -11,4 +11,8 @@ public class Calculator {
     public static float subtract (float number1 , float number2){
         return number1 - number2;
     }
+
+    public static float multiply (float number1, float number2) {return number1 * number2;}
+    public static float divide (float number1 , float number2) {return number1/number2;}
+
 }
--- src/Runner/Test.java
+++ src/Runner/Test.java
@@ -6,6 +6,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(Calculator.sum(30,15));
         System.out.println(Calculator.subtract(10,15));
+        System.out.println(Calculator.multiply(10,15));
+        System.out.println(Calculator.divide(20,15));
     }
 
 }
