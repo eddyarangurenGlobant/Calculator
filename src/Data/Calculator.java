1d33320e3316e8de8117771fa58488f0cd5679ea
eddyarangurenGlobant <eddy.aranguren@globant.com>
2022-04-20 00:05:47 -0500

    Adding a message to the sum and subtract methods

--- src/Data/Calculator.java
+++ src/Data/Calculator.java
@@ -2,8 +2,9 @@ package Data;
 
 public class Calculator {
     public Calculator(){
-
     }
+
+    // These are the basic calculator functions
     public static float sum (float number1, float number2){
         return number1 + number2;
     }
